77d5c3bafdd27959b45f223cef714f20cd5b9d94
Antony <ac.chan@live.com>
2021-08-17 05:52:56 +0000

    fix syntax error and finish excerise 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,8 +4,8 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, men_apparel: true, women_apparel: true)
-richmond = Store.create(name: "Richmond", annual_revenue: 1260000, men_apparel: false, women_apparel: true)
-gastown = Store.create(name: "Gastown", annual_revenue: 190000, men_apparel: true, women_apparel: false)
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 puts "The database has #{Store.count} stores."
