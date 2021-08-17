8bd98beddb7e2db83cb731a46a9c4069fa1f5d29
Antony <ac.chan@live.com>
2021-08-17 05:12:10 +0000

    excercise 1 done

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,8 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, men_apparel: true, women_apparel: true)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, men_apparel: false, women_apparel: true)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, men_apparel: true, women_apparel: false)
+
+puts "The database has #{Store.count} stores."
